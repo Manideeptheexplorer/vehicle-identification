443e08802ec206ab9f92f9ac07f2fa44c80f2b01
Manideeptheexplorer <venkatkotha828@gmail.com>
2024-11-25 13:39:19 +0530

    fixed the ui

--- vehiclerecognition/vehiclerecognition/detection/forms.py
+++ vehiclerecognition/vehiclerecognition/detection/forms.py
@@ -9,3 +9,17 @@ class ImageUploadForm(forms.Form):
     })
     )
     
+class VideoUploadForm(forms.Form):
+    video = forms.FileField(label="Upload a video file")
+
+class CustomAuthenticationForm(forms.Form):
+    username_or_email = forms.CharField(
+        label="Username or Email",
+        widget=forms.TextInput(attrs={'placeholder': 'Username or Email', 'class': 'form-control'})
+    )
+    password = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control'})
+    )
+
+
